c93564be214431e3b9f5d564907663780183abe7
Layendan <layendan.h@gmail.com>
2021-10-02 01:41:51 -0700

    added audio reading

--- discordclip/src/main/java/org/sunhacks/App.java
+++ discordclip/src/main/java/org/sunhacks/App.java
@@ -18,6 +18,20 @@ import net.dv8tion.jda.api.managers.AudioManager;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import net.dv8tion.jda.api.audio.AudioReceiveHandler;
+import net.dv8tion.jda.api.audio.CombinedAudio;
+
 import javax.security.auth.login.LoginException;
 import java.util.EnumSet;
 
@@ -25,7 +39,12 @@ import java.util.EnumSet;
  * Hello world!
  *
  */
-public class App extends ListenerAdapter {
+public class App extends ListenerAdapter implements AudioReceiveHandler {
+    private byte[] opusAudio;
+    private byte[] wavAudio;
+
+    private final Queue<byte[]> queue = new ConcurrentLinkedQueue<>();
+
     public static void main(String[] args) throws LoginException {
         JDA jda = JDABuilder
                 .createLight(System.getenv("TOKEN"),
@@ -65,6 +84,7 @@ public class App extends ListenerAdapter {
                     event.reply("I am not allowed to join voice channels");
                 }
                 else {
+                    voiceChannel.getBitrate();
                     audioManager.openAudioConnection(voiceChannel);
                 }
                 break;
@@ -154,4 +174,91 @@ public class App extends ListenerAdapter {
                 .queue();
     }
 
+    private void rawToWave(final File rawFile, final File waveFile, int bitrate) throws IOException {
+
+        byte[] rawData = new byte[(int) rawFile.length()];
+        DataInputStream input = null;
+        try {
+            input = new DataInputStream(new FileInputStream(rawFile));
+            input.read(rawData);
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+        }
+    
+        DataOutputStream output = null;
+        try {
+            output = new DataOutputStream(new FileOutputStream(waveFile));
+            // WAVE header
+            // see http://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+            writeString(output, "RIFF"); // chunk id
+            writeInt(output, 36 + rawData.length); // chunk size
+            writeString(output, "WAVE"); // format
+            writeString(output, "fmt "); // subchunk 1 id
+            writeInt(output, 16); // subchunk 1 size
+            writeShort(output, (short) 1); // audio format (1 = PCM)
+            writeShort(output, (short) 1); // number of channels
+            writeInt(output, 44100); // sample rate
+            writeInt(output, bitrate / 8); // byte rate
+            writeShort(output, (short) 2); // block align
+            writeShort(output, (short) 16); // bits per sample
+            writeString(output, "data"); // subchunk 2 id
+            writeInt(output, rawData.length); // subchunk 2 size
+            // Audio data (conversion big endian -> little endian)
+            short[] shorts = new short[rawData.length / 2];
+            ByteBuffer.wrap(rawData).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shorts);
+            ByteBuffer bytes = ByteBuffer.allocate(shorts.length * 2);
+            for (short s : shorts) {
+                bytes.putShort(s);
+            }
+    
+            output.write(fullyReadFileToBytes(rawFile));
+        } finally {
+            if (output != null) {
+                output.close();
+            }
+        }
+    }
+        byte[] fullyReadFileToBytes(File f) throws IOException {
+        int size = (int) f.length();
+        byte bytes[] = new byte[size];
+        byte tmpBuff[] = new byte[size];
+        FileInputStream fis= new FileInputStream(f);
+        try { 
+    
+            int read = fis.read(bytes, 0, size);
+            if (read < size) {
+                int remain = size - read;
+                while (remain > 0) {
+                    read = fis.read(tmpBuff, 0, remain);
+                    System.arraycopy(tmpBuff, 0, bytes, size - remain, read);
+                    remain -= read;
+                } 
+            } 
+        }  catch (IOException e){
+            throw e;
+        } finally { 
+            fis.close();
+        } 
+    
+        return bytes;
+    } 
+    private void writeInt(final DataOutputStream output, final int value) throws IOException {
+        output.write(value >> 0);
+        output.write(value >> 8);
+        output.write(value >> 16);
+        output.write(value >> 24);
+    }
+    
+    private void writeShort(final DataOutputStream output, final short value) throws IOException {
+        output.write(value >> 0);
+        output.write(value >> 8);
+    }
+    
+    private void writeString(final DataOutputStream output, final String value) throws IOException {
+        for (int i = 0; i < value.length(); i++) {
+            output.write(value.charAt(i));
+        }
+    }
 }
